8ba8726563b57ec5df3c0bbc878eb548df0b3241
Tausif Ansari <169084613+tausifAnsari9@users.noreply.github.com>
2024-05-10 18:25:52 +0000

    numbers

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -4,3 +4,27 @@ const age = 18
 //console.log();
 
 console.log(`hello my name is ${name} and my age is ${age}`);
+
+const gameName = "parrot partot";
+
+console.log(gameName[0]);
+
+console.log(gameName.length);
+
+console.log(gameName.__proto__);
+
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+
+console.log(gameName.charAt(9));
+
+const newName = "       tausif     ";
+
+console.log(newName.trim());
+
+const id = "tausofan567@gamil.com"
+console.log(id.replace('o','i'));
+console.log(id.includes('@'));
+
+ const newCall = console.log(id.split('.'));
+ console.log(typeof newCall);
