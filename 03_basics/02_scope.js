f0bdf94fc9113895ac5e859f2620096bc3f93492
Tausif Ansari <169084613+tausifAnsari9@users.noreply.github.com>
2024-07-03 11:13:15 +0000

    scope basics

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -6,3 +6,45 @@ if(true){  //block scope
 }
 
 // console.log(a);
+
+function one(){
+    const userName = "tausif"
+     function two(){
+        const website = "youtube"
+        console.log(userName);
+     }
+     // console.log(website);
+    // two()
+}
+
+//one();
+
+
+if (true) {
+    const userName2 = "tauhid"
+    if (userName2 === "tauhid") {
+        const website = " youtube"
+       // console.log(userName2 + website);
+        
+    }
+   // console.log(website);
+    
+}
+// console.log(userName2);
+
+
+
+// ------------------------------ INTERSTING-----------------------------
+
+// console.log(addOne(1));
+function addOne(num) {
+    return num + 1
+}
+
+
+
+ addTwo(2) // give error because of exicuting before declaration
+const addTwo = function addTwo(num2) {
+    return num2 + 2
+}
+
