cb5f2507b715fc6bbbe143023cde6d346ff6d43b
Tausif Ansari <169084613+tausifAnsari9@users.noreply.github.com>
2024-11-14 10:59:08 +0000

    practice

---
+++ 07_problems-practice/prob_1.js
@@ -0,0 +1,37 @@
+// for (let index = 0; index <=10; index++) {
+//     const x=index;
+//     if(x%2==0){
+
+//         console.log(x);
+//     }
+    
+// }
+
+
+// multiplication table
+
+// for (let index = 1; index <= 10 ; index++) {
+   
+//    for (let jndex = 1; jndex <=10; jndex++) {
+//      console.log(`${index} time ${jndex} are ${index*jndex}`);
+     
+     
+    
+//    } 
+//    console.log("\n");
+// }
+
+// let lenghtInKm = 100;
+// const miles = lenghtInKm*0.621;
+
+// console.log(`${lenghtInKm}km = ${miles}miles`);
+
+
+const arr = [1,2,8,-11,88,2.09]
+
+// arr.sort((a,b)=> a-b)
+
+// console.log(arr.sort());
+
+
+
